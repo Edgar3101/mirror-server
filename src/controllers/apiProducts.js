221d03c0776169bcd3d81cfba442fd8e3614cd5d
Edgar3101 <edgar.hernandez3101@gmail.com>
2022-01-27 22:02:23 -0500

    Ready API

--- src/controllers/apiProducts.js
+++ src/controllers/apiProducts.js
@@ -1,5 +1,6 @@
+import sequelize from "../db/connection";
 import Product from "../models/Product";
-
+import Variant from "../models/Variant";
 
 export async function GetProducts(req, res){
 
@@ -9,7 +10,26 @@ export async function GetProducts(req, res){
 export async function getCountofProduct(req, res){
     const query = await Product.count();
     res.json({query})
+}
+export async function getRandomProducts(req, res){
+    //Este modulo tiene el objetivo de entregar productos aleatorios al espejo
+    const query= await Product.findAll({
+    order: sequelize.random(),
+    limit: 8
+    
+    })
+    res.json({query})
 
+}
 
-  
+export async function getVariantOfProduct(req, res){
+    //Este modulo tiene el objetivo de entregar las variantes segun el id del producto
+    const query= await Variant.findAll({
+        where: {
+            productId: req.params.id
+        }
+    
+    })
+    res.json({query})
+
 }
--- src/routers/api.js
+++ src/routers/api.js
@@ -1,11 +1,13 @@
 import {Router} from "express";
 const router = Router();
 
-import {GetProducts, getCountofProduct} from "../controllers/apiProducts.js"
+import {GetProducts, getCountofProduct, getRandomProducts, getVariantOfProduct} from "../controllers/apiProducts.js"
 
 
 router.get("/", GetProducts );
 router.get("/count", getCountofProduct);
+router.get("/random", getRandomProducts);
+router.get("/variant/:id", getVariantOfProduct);
 
 
 
