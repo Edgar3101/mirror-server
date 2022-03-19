106f01b6b64d3b525af09cd268805b2f61877ffa
Edgar3101 <edgar.hernandez3101@gmail.com>
2022-03-19 15:11:19 -0500

    change for async support

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,7 @@
-const express = require("express")
+import express from "express";
+import Product from "./models/Product";
+import VariantColor from "./models/Variant_Color";
+import Variant_Size from "./models/Variant_Size";
 const http = require("http")
 const WebSocket= require("ws")
 
@@ -13,9 +16,12 @@ const wss = new WebSocket.Server({ server });
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message, isBinary) {
     console.log(message.toString(), isBinary);
-    wss.clients.forEach(function each(client) {
+    wss.clients.forEach(async function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
+        var buffer= JSON.parse(message.toString())
+        var data= await searchData(buffer)
+        console.log(data)
+        client.send(JSON.stringify(data));
       }
     });
   });
@@ -26,3 +32,24 @@ wss.on("connection", function connection(ws) {
 server.listen(8002, () => {
     console.log(`Server started on port ${server.address().port} :)`);
 });
+
+async function searchData(buffer){
+  const size= await Variant_Size.findOne({
+    where: {
+      id: buffer.message.id
+    }
+  })
+  const color= await VariantColor.findOne({
+    where: {
+      id: size.variant_color_id
+    }
+  })
+  const product= await Product.findOne({
+    where: {
+      id: color.productId
+    }
+  })
+
+  return { id: product.dataValues.id, title: product.dataValues.title, description: product.dataValues.description, 
+    color: color.dataValues.color, size: size.dataValues.size}
+}
